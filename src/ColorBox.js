281001e8c0fa2f36b33af036f5f05ae69315e8b6
Artem <artem@artemsMac.local>
2020-10-21 16:42:29 +0200

    Adjust text color based on luminance

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { Link } from "react-router-dom";
+import chroma from "chroma-js";
 import "./ColorBox.css";
 
 class ColorBox extends Component {
@@ -21,6 +22,8 @@ class ColorBox extends Component {
   render() {
     const { name, background, moreUrl, showLink } = this.props;
     const { copied } = this.state;
+    const textColor = chroma(background).luminance() > 0.5 ? "#000" : "#fff";
+    const textShadowColor = chroma(background).luminance() > 0.5 ? "#fff" : "#000";
     return (
       <CopyToClipboard text={background} onCopy={this.changeCopyState}>
         <div style={{ backgroundColor: background }} className="ColorBox">
@@ -28,19 +31,26 @@ class ColorBox extends Component {
             style={{ backgroundColor: background }}
             className={`copy-overlay ${copied && "show"}`}
           />
-          <div className={`copy-msg ${copied && "show"}`}>
-            <h1>Copied!</h1>
+          <div
+            style={{ color: textColor }}
+            className={`copy-msg ${copied && "show"}`}
+          >
+            <h1 style={{ textShadow: `1px 2px ${textShadowColor}` }}>Copied!</h1>
             <p>{background}</p>
           </div>
           <div className="copy-container">
-            <div className="box-content">
+            <div style={{ color: textColor }} className="box-content">
               <span>{name}</span>
             </div>
-            <button className="copy-button">Copy</button>
+            <button style={{ color: textColor }} className="copy-button">
+              Copy
+            </button>
           </div>
           {showLink && (
             <Link to={moreUrl} onClick={(e) => e.stopPropagation()}>
-              <span className="see-more">More</span>
+              <span style={{ color: textColor }} className="see-more">
+                More
+              </span>
             </Link>
           )}
         </div>
