5bc534724404494e2b5ad65c28d48b0083511be0
Artem <artem@artemsMac.local>
2020-10-26 14:18:18 +0100

    Save palettes to local storage

--- src/App.js
+++ src/App.js
@@ -10,8 +10,9 @@ import NewPaletteForm from "./NewPaletteForm";
 class App extends React.Component {
   constructor(props) {
     super(props);
+    const savedPalettes = JSON.parse(window.localStorage.getItem("palettes"));
     this.state = {
-      palettes: seedColors,
+      palettes: savedPalettes || seedColors,
     };
     this.findPalette = this.findPalette.bind(this);
     this.savePalette = this.savePalette.bind(this);
@@ -24,7 +25,18 @@ class App extends React.Component {
   }
 
   savePalette(newPalette) {
-    this.setState({ palettes: [...this.state.palettes, newPalette] });
+    this.setState(
+      { palettes: [...this.state.palettes, newPalette] },
+      this.syncLocalStorage
+    );
+  }
+
+  syncLocalStorage() {
+    // save palettes to local storage
+    window.localStorage.setItem(
+      "palettes",
+      JSON.stringify(this.state.palettes)
+    );
   }
 
   render() {
