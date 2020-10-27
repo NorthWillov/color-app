1b83599bd3871a0fe61b3face5d96a7ae248178a
Artem <artem@artemsMac.local>
2020-10-27 18:45:17 +0100

    Fix issues with NewPaletteForm

--- src/styles/DraggableColorBoxStyles.js
+++ src/styles/DraggableColorBoxStyles.js
@@ -1,3 +1,4 @@
+import chroma from "chroma-js";
 import sizes from "./sizes";
 
 const styles = {
@@ -11,20 +12,20 @@ const styles = {
     marginBottom: "-3.5px",
     "&:hover svg": {
       color: "white",
-      transform: "scale(1.5)"
+      transform: "scale(1.5)",
     },
     [sizes.down("lg")]: {
       width: "25%",
-      height: "20%"
+      height: "20%",
     },
     [sizes.down("md")]: {
       width: "50%",
-      height: "10%"
+      height: "10%",
     },
     [sizes.down("sm")]: {
       width: "100%",
-      height: "5%"
-    }
+      height: "5%",
+    },
   },
   boxContent: {
     position: "absolute",
@@ -32,16 +33,17 @@ const styles = {
     left: "0px",
     bottom: "0px",
     padding: "10px",
-    color: "rgba(0, 0, 0, 0.5)",
     letterSpacing: "1px",
+    color: (props) =>
+      chroma(props.color).luminance() <= 0.08 ? "#fff" : "#000",
     textTransform: "uppercase",
     fontSize: "12px",
     display: "flex",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
   },
   deleteIcon: {
-    transition: "all 0.3s ease-in-out"
-  }
+    transition: "all 0.3s ease-in-out",
+  },
 };
 
 export default styles;
