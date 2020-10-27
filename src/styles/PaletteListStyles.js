f362e58f35fdd9a2884d5aca2918fd69f3e073ec
Artem <artem@artemsMac.local>
2020-10-27 11:56:57 +0100

    Add bg svg to PaletteList

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,12 +1,16 @@
 import sizes from "./sizes";
+import bg from "./bg.svg";
 
 export default {
   root: {
-    backgroundColor: "blue",
+    /* background by SVGBackgrounds.com */
+    backgroundColor: "#1407aa",
+    backgroundImage: `url(${bg})`,
     height: "100vh",
     display: "flex",
     alignItems: "flex-start",
     justifyContent: "center",
+    overflow: "scroll",
   },
   container: {
     width: "50%",
@@ -42,7 +46,7 @@ export default {
     },
     [sizes.down("xs")]: {
       gridTemplateColumns: "repeat(1, 100%)",
-      gridGap: "1rem",
+      gridGap: "1.4rem",
     },
   },
 };
