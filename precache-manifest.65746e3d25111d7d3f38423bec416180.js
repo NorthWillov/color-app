2671a363a801e7ea35d44cc31cb02110dda1a69b
Artem <artem@artemsMac.local>
2020-10-28 10:06:47 +0100

    Updates

---
+++ precache-manifest.65746e3d25111d7d3f38423bec416180.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e53865872972cb7dcf8ae374a1694bb2",
+    "url": "/colors-app/index.html"
+  },
+  {
+    "revision": "ca1843706a611928348d",
+    "url": "/colors-app/static/css/2.0da342d8.chunk.css"
+  },
+  {
+    "revision": "45cea6be5d4caaa60d8f",
+    "url": "/colors-app/static/css/main.127850da.chunk.css"
+  },
+  {
+    "revision": "ca1843706a611928348d",
+    "url": "/colors-app/static/js/2.4abb9f5b.chunk.js"
+  },
+  {
+    "revision": "2f5d88cd7a9fe173e498156fa6f65024",
+    "url": "/colors-app/static/js/2.4abb9f5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45cea6be5d4caaa60d8f",
+    "url": "/colors-app/static/js/main.01fb8c94.chunk.js"
+  },
+  {
+    "revision": "0d59c216cd7db2f95b86",
+    "url": "/colors-app/static/js/runtime-main.aad9f821.js"
+  },
+  {
+    "revision": "77428cd498bf82179ff99fdb246b2494",
+    "url": "/colors-app/static/media/bg.77428cd4.svg"
+  }
+]);
